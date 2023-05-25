62edb5cbf86fb1c40523e4c5ce868cdfe9e1efcb
VerticalHeretic <ls.warss98@gmail.com>
2023-05-25 16:34:48 +0200

    Initial Commit

---
+++ TILApp-iOS/TILApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILApp_iOSApp.swift
+//  TILApp-iOS
+//
+//  Created by Łukasz Stachnik on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TILApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
